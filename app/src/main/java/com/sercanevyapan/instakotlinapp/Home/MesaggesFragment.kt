d44b28edbc13eaaeaa1c6a895586a247d92ea066
dharma <dharmadh@gmail.com>
2019-01-25 15:22:56 +0300

    homeActivity için fragmentlerin oluşturulması

---
+++ app/src/main/java/com/sercanevyapan/instakotlinapp/Home/MesaggesFragment.kt
@@ -0,0 +1,19 @@
+package com.sercanevyapan.instakotlinapp.Home
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sercanevyapan.instakotlinapp.R
+
+
+class MesaggesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        var view=inflater?.inflate(R.layout.fragment_mesagges, container, false)
+        return view
+    }
+
+}
