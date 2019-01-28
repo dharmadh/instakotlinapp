b20c2337871643e91eaa87037ff5462aac5bfb8e
dharma <dharmadh@gmail.com>
2019-01-28 15:45:59 +0300

    19. Resimler için Universal Image Loader Kütüphanesi ve Ayarları

--- app/src/main/java/com/sercanevyapan/instakotlinapp/Profile/ProfileEditFragment.kt
+++ app/src/main/java/com/sercanevyapan/instakotlinapp/Profile/ProfileEditFragment.kt
@@ -6,14 +6,14 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.nostra13.universalimageloader.core.ImageLoader
 
 import com.sercanevyapan.instakotlinapp.R
+import com.sercanevyapan.instakotlinapp.utils.UniversalImageLoader
+import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.fragment_profile_edit.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -21,18 +21,38 @@ private const val ARG_PARAM2 = "param2"
  */
 class ProfileEditFragment : Fragment() {
 
+    lateinit var circleProfileImageFragment:CircleImageView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?): View? {
 
-        val view = inflater.inflate(R.layout.fragment_profile_edit, container, false)
+        val view = inflater!!.inflate(R.layout.fragment_profile_edit, container, false)
+
+        circleProfileImageFragment=view.findViewById(R.id.circleProfileImage)
+
+        initImageLoader()
+        setupProfilePicture()
 
         view.imgClose.setOnClickListener {
             activity!!.onBackPressed()
         }
 
         return view
+    }
+
+    private fun initImageLoader(){
+
+        var universalImageLoader=UniversalImageLoader(activity!!)
+        ImageLoader.getInstance().init(universalImageLoader.config)
+
+    }
+
+    private fun setupProfilePicture() {
+        //http://icons.iconarchive.com/icons/danleech/simple/1024/android-icon.png
+
+        var imgURL = "icons.iconarchive.com/icons/danleech/simple/1024/android-icon.png"
+        UniversalImageLoader.setImage(imgURL,circleProfileImageFragment,null,"http://")
     }
 
 
