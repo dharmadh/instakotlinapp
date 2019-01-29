f47c3320849900528b207ebe7856208099b1d98f
dharmadh <2103050031dharma>
2019-01-29 23:59:17 +0300

    22. Register Ekranı Tasarımı Bölüm 2 (Tekrar bakılacak)

--- app/src/main/java/com/sercanevyapan/instakotlinapp/Login/RegisterActivity.kt
+++ app/src/main/java/com/sercanevyapan/instakotlinapp/Login/RegisterActivity.kt
@@ -2,12 +2,74 @@ package com.sercanevyapan.instakotlinapp.Login
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.view.InputDevice
+import android.view.View
 import com.sercanevyapan.instakotlinapp.R
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        init()
+    }
+
+    private fun init() {
+        tvEposta.setOnClickListener {
+            viewTelefon.visibility= View.INVISIBLE
+            viewEposta.visibility=View.VISIBLE
+            etGirisYöntemi.setText("")
+            etGirisYöntemi.inputType=InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            etGirisYöntemi.setHint("E-posta")
+
+            btnIleri.isEnabled=false;
+            btnIleri.setTextColor(ContextCompat.getColor(this@RegisterActivity,R.color.sonukmavi))
+            btnIleri.setBackgroundColor(R.drawable.register_button)
+        }
+
+        tvTelefon.setOnClickListener {
+            viewTelefon.visibility= View.VISIBLE
+            viewEposta.visibility=View.INVISIBLE
+            etGirisYöntemi.setText("")
+            etGirisYöntemi.inputType=InputType.TYPE_CLASS_NUMBER
+            etGirisYöntemi.setHint("Telefon")
+
+            btnIleri.isEnabled=false;
+            btnIleri.setTextColor(ContextCompat.getColor(this@RegisterActivity,R.color.sonukmavi))
+            btnIleri.setBackgroundColor(R.drawable.register_button)
+        }
+
+        etGirisYöntemi.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(start+before+count>=10){
+
+                    btnIleri.isEnabled=true;
+                    btnIleri.setTextColor(ContextCompat.getColor(this@RegisterActivity,R.color.beyaz))
+                    btnIleri.setBackgroundColor(R.drawable.register_button_aktif)
+
+                }else{
+                    btnIleri.isEnabled=false;
+                    btnIleri.setTextColor(ContextCompat.getColor(this@RegisterActivity,R.color.sonukmavi))
+                    btnIleri.setBackgroundColor(R.drawable.register_button)
+                }
+            }
+
+
+        })
     }
 }
