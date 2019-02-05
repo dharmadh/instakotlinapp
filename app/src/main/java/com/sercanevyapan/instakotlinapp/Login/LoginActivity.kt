2b6a9bcc81c4aac7054aa70e8227ee7ee3846256
dharma <dharmadh@gmail.com>
2019-02-05 16:09:35 +0300

    36. Email Username veya TelNo ile Giriş Yapma

--- app/src/main/java/com/sercanevyapan/instakotlinapp/Login/LoginActivity.kt
+++ app/src/main/java/com/sercanevyapan/instakotlinapp/Login/LoginActivity.kt
@@ -5,25 +5,104 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.sercanevyapan.instakotlinapp.Models.Users
 import com.sercanevyapan.instakotlinapp.R
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    lateinit var mAuth: FirebaseAuth
+    lateinit var mRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        mAuth = FirebaseAuth.getInstance()
+        mRef = FirebaseDatabase.getInstance().reference
         init()
     }
 
-    fun init(){
+    fun init() {
         etEmailTelorUsername.addTextChangedListener(watcher)
         etSifreLogin.addTextChangedListener(watcher)
 
+        btnGirisYap.setOnClickListener {
+            oturumAcacakKullaniciyiDenetle(etEmailTelorUsername.text.toString(), etSifreLogin.text.toString())
+        }
+
     }
 
-    var watcher:TextWatcher=object : TextWatcher{
+    private fun oturumAcacakKullaniciyiDenetle(emailPhoneNumberUserName: String, sifre: String) {
+
+        mRef.child("users").orderByChild("email").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError?) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot?) {
+                for (ds in p0!!.children) {
+                    var okunanKullanici = ds.getValue(Users::class.java)
+
+                    if (okunanKullanici!!.email!!.toString().equals(emailPhoneNumberUserName)) {
+
+                        oturumAc(okunanKullanici, sifre, false)
+                        break
+
+                    } else if (okunanKullanici!!.user_name!!.toString().equals(emailPhoneNumberUserName)) {
+
+                        oturumAc(okunanKullanici, sifre, false)
+                        break
+
+                    } else if (okunanKullanici!!.phone_number!!.toString().equals(emailPhoneNumberUserName)) {
+
+                        oturumAc(okunanKullanici, sifre, true)
+                        break
+
+                    }
+
+                }
+            }
+
+        })
+
+    }
+
+    private fun oturumAc(okunanKullanici: Users, sifre: String, telefonIleGiris: Boolean) {
+
+        var girisYapacakEmail = ""
+
+        if (telefonIleGiris == true) {
+            girisYapacakEmail = okunanKullanici.email_phone_number.toString()
+        } else {
+            girisYapacakEmail = okunanKullanici.email.toString()
+        }
+
+        mAuth.signInWithEmailAndPassword(girisYapacakEmail, sifre)
+            .addOnCompleteListener(object : OnCompleteListener<AuthResult> {
+                override fun onComplete(p0: Task<AuthResult>) {
+                    if (p0!!.isSuccessful) {
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Oturum açıldı :" + mAuth.currentUser!!.uid,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(this@LoginActivity, "Kullanıcı Adı/Şifre Hatalı :", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            })
+
+    }
+
+    var watcher: TextWatcher = object : TextWatcher {
         override fun afterTextChanged(s: Editable?) {
 
         }
@@ -33,13 +112,13 @@ class LoginActivity : AppCompatActivity() {
         }
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            if(etEmailTelorUsername.text.toString().length >= 6 && etSifreLogin.text.toString().length >=6){
-                btnGirisYap.isEnabled=true
-                btnGirisYap.setTextColor(ContextCompat.getColor(this@LoginActivity!!,R.color.beyaz))
+            if (etEmailTelorUsername.text.toString().length >= 6 && etSifreLogin.text.toString().length >= 6) {
+                btnGirisYap.isEnabled = true
+                btnGirisYap.setTextColor(ContextCompat.getColor(this@LoginActivity!!, R.color.beyaz))
                 btnGirisYap.setBackgroundResource(R.drawable.register_button_aktif)
-            }else{
-                btnGirisYap.isEnabled=false
-                btnGirisYap.setTextColor(ContextCompat.getColor(this@LoginActivity!!,R.color.sonukmavi))
+            } else {
+                btnGirisYap.isEnabled = false
+                btnGirisYap.setTextColor(ContextCompat.getColor(this@LoginActivity!!, R.color.sonukmavi))
                 btnGirisYap.setBackgroundResource(R.drawable.register_button)
             }
         }
